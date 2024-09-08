59f5164ff6c10dac704fbcfe9bc796a978c9f8ab
Maxime Pauvert <maximepvrt@gmail.com>
2024-09-08 19:13:30 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+      '@nuxt/content',
+      '@nuxt/ui',
+      '@nuxthq/studio',
+  ],
+    studio: {
+        enabled: true
+    }
+})
